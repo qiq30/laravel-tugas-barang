5dc76b94d765bcf6450a847bdecf78a3b417aee2
Qiiqqq <sniqi87@gmail.com>
2025-03-06 17:54:32 +0800

    Deskripsi perubahan nav barang responsif

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,8 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('barang')" :active="request()->routeIs('barang')">
                         {{ __('Barang') }}
                     </x-nav-link>
